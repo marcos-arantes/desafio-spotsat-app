52399a2c09b60201227faef139139831f8bd0fa5
Marcos <marcosarantes.contato@hotmail.com>
2024-06-02 21:09:50 -0300

    teste

--- app/page.tsx
+++ app/page.tsx
@@ -16,6 +16,9 @@ const HomePage: React.FC<Props> = () => {
 
       <main>
         <h1>Bem-vindo à minha aplicação simples usando Next.js com TypeScript!</h1>
+        <br>
+        
+        </br>
         <h2>Front-End com Next.js - Desafio SpotSat</h2>
         <img src="https://spotsat.com.br/wp-content/uploads/2023/03/LogoS-1024x304.webp" alt="spotsat" style={{ width: '100%', maxWidth: '500px' }} />
       </main>
